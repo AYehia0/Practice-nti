99d5790c188bf0be175731e57a35167c866ece8e
AYehia0 <ahmedyehia611@gmail.com>
2021-09-05 10:37:04 +0200

    added dynamic way to get the info using the node terminal

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+const readline = require("readline");
 
+// readline input/out
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+// getting the user info using node 
+function getUserInput() {
+
+    rl.question("what is your name ? ", name =>{
+        rl.question("what is your age ? ", age => {
+            rl.question("what is your salary ? ", salary=>{
+                const userLog = `Your name is ${name}, your salary is ${salary} and you're ${age} years old`
+                console.log(userLog)
+
+                // closing ...
+                rl.close()
+            })
+        })
+
+    })
+
+}
 // display names, salary and etc
-
 function showUserInfo(name, age, salary){
 
     const userLog = `Your name is ${name}, your salary is ${salary} and you're ${age} years old`
 
     console.log(userLog)
 }
+
+getUserInput()
