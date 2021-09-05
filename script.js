af9e034658ffcc279415922e08f1ed65a2dafe28
AYehia0 <ahmedyehia611@gmail.com>
2021-09-05 10:14:46 +0200

    simple function to display the info

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// display names, salary and etc
+
+function showUserInfo(name, age, salary){
+
+    const userLog = `Your name is ${name}, your salary is ${salary} and you're ${age} years old`
+
+    console.log(userLog)
+}
