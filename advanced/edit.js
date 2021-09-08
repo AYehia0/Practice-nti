bb6b7a8202368cb8b0d3dc7566663c75711f36fc
AYehia0 <ahmedyehia611@gmail.com>
2021-09-08 10:39:57 +0200

    checking for titles

--- advanced/edit.js
+++ advanced/edit.js
@@ -1,11 +1,28 @@
 
 
+// object is sent
+// note it only works with the title,,, very specific function
+function validateTitle(task, allTasks){
+
+    const title = task.title
+
+    allTasks.forEach(t => {
+        if (title == t.title) return false 
+
+    })
+
+    return true
+    
+}
+
+
 const editNoteById = (noteId, editKey, editVal) => {
     // getting the note's index
 
     const noteIndex = getNoteIndex(noteId)
 
-    // u can't edit the id lol
+    // u can't edit the id, date lol
+    // no validation is set, for now
     const notes = readNotesFromWhatever()
 
     const targetNote = notes[noteIndex]
