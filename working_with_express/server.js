57c5ceb3a5b957ee5c26ef30a9c31a29bfcae779
AYehia0 <ahmedyehia611@gmail.com>
2021-09-09 11:18:11 +0200

    servering static files

--- working_with_express/server.js
+++ working_with_express/server.js
@@ -1,11 +1,14 @@
 const config = require('./config')
 const express = require('express')
+const path = require('path')
 
 
 const PORT = config.configs.PORT
 const app = express()
 
 
+// middlewares 
+app.use('/static', express.static(path.join(__dirname, "public")))
 
 app.get('/', (req, res, err) => {
     res.send("<h1>Hello Darkness My Old Friend</h1>")
