3f0dca9e96f7eab1458d6deff4825b18c2c53f9a
AYehia0 <ahmedyehia611@gmail.com>
2021-09-13 13:17:43 +0200

    adding to json file

--- user_app/routes/addRoute.js
+++ user_app/routes/addRoute.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const fileHandler = require('../moduleX')
+
+const fileName = "models/users.json"
 
 router.get('/', (req, res, err) => {
     res.render('add')
+})
+
+router.post('/', (req, res, err) => {
+    const userData = req.body
+
+    // doing processes on the data, whatever
+    fileHandler.addToFile(fileName, userData)
+    res.redirect('/')
 })
 
 module.exports = router
