22e5f5331af8010a37af8d1c2cfaac75002a1757
AYehia0 <ahmedyehia611@gmail.com>
2021-09-16 13:38:27 +0200

    adding user and searching by id

--- working_with_dbs/controller/mainController.js
+++ working_with_dbs/controller/mainController.js
@@ -1,8 +1,37 @@
+const user = require('../models/userModel')
 
 const mainRoute = (req, res, err) => {
     res.send("Main Route !!!")
 }
 
+const addUser = (req, res, err) => {
+
+    // getting the user from the body
+
+    new user(req.body)
+        .save()
+        .then((u) => res.send(`User has been added: ${u}`))
+        .catch(e => res.send(e.message))
+}
+
+const getUser = async (req, res, err) => {
+    const id = req.params.id
+
+    try{
+        const data = await user.findById(id)
+
+        if (!data)
+            res.send("User Not Found")
+
+        res.send(data)
+    }catch(e) {
+        res.send(e.message)
+    }
+}
+
+
 module.exports = {
-    mainRoute
+    mainRoute,
+    addUser,
+    getUser
 }
