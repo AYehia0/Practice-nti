25e68291a7cdb15defe89233e52c18f98217e470
AYehia0 <ahmedyehia611@gmail.com>
2021-09-16 13:39:41 +0200

    working with req.body

--- working_with_dbs/app.js
+++ working_with_dbs/app.js
@@ -4,10 +4,9 @@ const routes = require('./routes/mainRoutes')
 const app = express()
 require('./db/connection')
 
-
 // middlewares 
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use(routes)
-
 
 module.exports = app
