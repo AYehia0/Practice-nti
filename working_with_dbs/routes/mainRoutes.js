1ba031c329d5d7ddeb0fefffa245370694e7be1a
AYehia0 <ahmedyehia611@gmail.com>
2021-09-16 13:39:27 +0200

    the 2 routes

--- working_with_dbs/routes/mainRoutes.js
+++ working_with_dbs/routes/mainRoutes.js
@@ -4,4 +4,9 @@ const cont = require('../controller/mainController')
 // main route
 router.get('/', cont.mainRoute)
 
+
+// adding users route
+router.post('/add', cont.addUser)
+router.get('/get/:id', cont.getUser)
+
 module.exports = router
