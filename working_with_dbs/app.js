6a8f5e7a259e21cfc6b807c23f433545f2c960a4
AYehia0 <ahmedyehia611@gmail.com>
2021-09-16 11:05:49 +0200

    everything is org in an app file

--- working_with_dbs/app.js
+++ working_with_dbs/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const routes = require('./routes/mainRoutes')
+
 const app = express()
 require('./db/connection')
 
 
-
-
+// middlewares 
+app.use(express.json())
+app.use(routes)
 
 
 module.exports = app
